6d00a015bd42395bb3f7650fa7a438e7d285103b
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2015-06-09 11:03:59 +0200

    project setup

---
+++ MVCInterfacesBinding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCInterfaceBinding.Global" Language="C#" %>
---
+++ MVCInterfacesBinding/Models/HomeModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCInterfaceBinding.Models
+{
+    public class HomeModel
+    {
+        public List<IItem> ItemList { get; set; }
+    }
+
+    public interface IItem
+    {
+        string Name { get; set; }
+        int Value { get; set; }
+
+        string GetAllProps();
+    }
+
+    public class Item1 : IItem
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public int OtherValue { get; set; }
+
+
+        public string GetAllProps()
+        {
+            return string.Format("{0}-{1}-{2}", Name, Value, OtherValue);
+        }
+    }
+
+    public class Item2 : IItem
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public string OtherName { get; set; }
+
+        public string GetAllProps()
+        {
+            return string.Format("{0}-{1}-{2}", Name, Value, OtherName);
+        }
+    }
+}
